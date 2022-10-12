9704320f876e891b4e121548f058ef2442958635
Ericles Brum <ericlespbrum@gmail.com>
2022-10-11 22:15:33 -0300

    Alterações no html/css/javascript
    Adição da classe erroMensage, onde é exibido uma bordar para indicar o erro e outros estilos.
    Esconder essa mensagem de erro.
    Validação dos campos digitados
    em valores númericos, somente numeros acima de 0.01 podem ser digitados

--- javascript/main.js
+++ javascript/main.js
@@ -7,13 +7,17 @@ const Main={
     },
     cacheSelectors:function()
     {
+        /*Campos*/
         this.$name=document.querySelector("#name");
         this.$monthyFees=document.querySelector("#monthyFees");
         this.$interestRates=document.querySelector("#interestRates");
         this.$time=document.querySelector("#time");
         this.$buttonForm=document.querySelector("#buttonForm");
+        /*-------*/
+        this.$buttonRefresh=document.querySelector("#buttonRefresh");
         this.$formCiclic=document.querySelector("#formCiclic");
         this.$main=document.querySelector("#main");
+        this.$errorMensages=document.querySelectorAll(".erroMensage");
     },
     createTime:function()
     {
@@ -33,7 +37,7 @@ const Main={
                     sob uma taxa de juros de ${this.$interestRates.value}% ao mês.
                 </p>
             </article>
-            <button>Simular Novamente</button>
+            <button onClick="window.location.reload()">Simular Novamente</button>
         </div>
         `
     },
@@ -53,9 +57,52 @@ const Main={
     Events:{
         onclick_Simulated:function(e)
         {
-            e.preventDefault();
+            let missingInput=false;
             const self=this;
-            let _interestRates=parseFloat(this.$interestRates.value)/100;
+            let _interestRates=parseFloat(this.$interestRates.value.replace(',','.'))/100;
+            e.preventDefault();
+            if(this.$name.value=="")
+            {
+                this.$errorMensages[0].classList.remove("hidden");
+                this.$name.classList.add("border");
+                missingInput=true;
+            }
+            else{
+                this.$errorMensages[0].classList.add("hidden");
+                this.$name.classList.remove("border");
+            }
+            if(this.$monthyFees.value=="")
+            {
+                this.$errorMensages[1].classList.remove("hidden");
+                this.$monthyFees.classList.add("border");
+                missingInput=true;
+            }
+            else{
+                this.$errorMensages[1].classList.add("hidden");
+                this.$monthyFees.classList.remove("border");
+            }
+            if(this.$interestRates.value=="")
+            {
+                this.$errorMensages[2].classList.remove("hidden");
+                this.$interestRates.classList.add("border");
+                missingInput=true;
+            }
+            else{
+                this.$errorMensages[2].classList.add("hidden");
+                this.$interestRates.classList.remove("border");
+            }
+            if(this.$time.value=="")
+            {
+                this.$errorMensages[3].classList.remove("hidden");
+                this.$time.classList.add("border");
+                missingInput=true;
+            }
+            else{
+                this.$errorMensages[3].classList.add("hidden");
+                this.$time.classList.remove("border");
+            }
+            if(missingInput)
+                return;
             fetch("http://api.mathjs.org/v4/",{
                 method:"POST",
                 body: JSON.stringify(
